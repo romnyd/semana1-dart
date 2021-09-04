a09276f252ee9a401cfd46a85af1b39619f19609
romnyduarte <romnyduarte@outlook.com>
2021-09-04 08:19:32 -0500

    modificacion variables

--- 1.variables/variables.dart
+++ 1.variables/variables.dart
@@ -5,7 +5,17 @@ void main() {
   int edad_dos = 34;
   var test;
   test=100;
+
+  //null safety example
+  int? variableNull = null;
+  String? variableStringNull = null;
+
+  //final const
+  final String variableFinalTest = "hola";
+  final String variableConstTest = "hola";
   print(nombre_dos.toUpperCase());
   print(nombre);
   print(test);
+  print(variableFinalTest);
+  print(variableConstTest);
 }
