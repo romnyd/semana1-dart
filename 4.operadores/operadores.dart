9173861856c28e50b1b29eda8f5a5541798054ce
romnyduarte <romnyduarte@outlook.com>
2021-09-04 10:56:24 -0500

    operadores

---
+++ 4.operadores/operadores.dart
@@ -0,0 +1,9 @@
+void main(){
+  var variable1 = 5;
+  var varibale2 = 4.0;
+  var variable3 = variable1>5 ?'Si':'No';
+  var variable4;
+  var variable5= variable4 ?? "es falso";
+  print(variable3);
+  print(variable5);
+}
