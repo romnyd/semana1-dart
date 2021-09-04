a6ee2a0edf0284978ac6bf72360259d16a7f6bdd
romnyduarte <romnyduarte@outlook.com>
2021-09-04 08:13:16 -0500

    inicial de variables

---
+++ 1.variables/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  var nombre = "rommy";
+  String nombre_dos = "romny";
+  var edad = 89;
+  int edad_dos = 34;
+  var test;
+  test=100;
+  print(nombre_dos.toUpperCase());
+  print(nombre);
+  print(test);
+}
